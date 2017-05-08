8e44a3a00a0ea8e21ade7c35d918fbb49221ebc0
wkrt7 <wiktor7645@gmail.com>
2017-05-08 09:59:24 +0200

    Dodany pdf

--- main1.m
+++ main1.m
@@ -2,6 +2,6 @@ close all;
 
 Tau = [0 1.5 3 4 5.5];
 
-Y = fmincon(@f_celu, Tau, [-1 0 0 0 0 ;1 -1 0 0 0;0 -1 1 0 0; 0 0 1 -1 0; 0 0 0 -1 1],[0;0;0;0;0])
-% f_celu(Tau);
+% Y = fmincon(@f_celu, Tau, [-1 0 0 0 0 ;1 -1 0 0 0;0 -1 1 0 0; 0 0 1 -1 0; 0 0 0 -1 1],[0;0;0;0;0]);
+f_celu(Tau);
 hold off;
