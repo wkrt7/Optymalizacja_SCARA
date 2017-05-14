9da3f8d17a518e7a6dfeeb9c9e9523b206f22c89
wkrt7 <wiktor7645@gmail.com>
2017-05-14 11:43:07 +0200

    poprawione

--- main1.m
+++ main1.m
@@ -2,13 +2,15 @@ close all;
 
 Tau1 = [0 1.5 2.9 3.8 4.8 8];
 Tau = [0 1.5 2.9 3 4.8 8];
+Y = [0.0000    1.6492    3.2766    3.6706    5.2810    5.4538];
 A = [-1 0 0 0 0 0 ;1 -1 0 0 0 0;0 1 -1 0  0 0;0 0 1 -1 0 0; 0 0 0 1 -1 0;0 0 0 0 1 -1];
 b = [0;0;0;0;0;0];
-Aeq = [1 0 0 0 0 0]
-beq = [0]
-options = optimset('MaxFunEvals',2000);
-[Y,fval] = fmincon(@f_celu, Tau,A,b,Aeq,beq,[],[],[],options)
+Aeq = [1 0 0 0 0 0];
+beq = [0];
+options = optimset('MaxFunEvals',3000);
+% [Y,fval] = fmincon(@f_celu, Tau,A,b,Aeq,beq,[],[],[],options);
+
 
 przedzialy_rys(Y);
-przedzialy_rys(Tau1)
+% przedzialy_rys(Tau1);
 
